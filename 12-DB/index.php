70660436f38c03ecc3d0e5a77e7a73cc4bd9b848
j2deme <j2deme@gmail.com>
2019-04-02 10:21:48 -0600

    Implementa ejemplo de ejecución de clase DB

--- 12-DB/index.php
+++ 12-DB/index.php
@@ -1,3 +1,16 @@
 <?php
 include_once "db.php";
+
+$db = new DB("root", "root12345", "demo");
+//var_dump($db);
+
+$usuarios = $db->select("usuarios");
+//var_dump($usuarios);
+
+if(count($usuarios) == 2){
+  $db->save("usuarios", "nombre,password,usuario", "'Francisco Lopez','12345','flopez'");
+}
+
+$ultimo = $db->first("usuarios","*","usuario = 'flopez'");
+var_dump($ultimo);
 ?>
